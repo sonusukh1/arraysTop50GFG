a7325b138c3cc5a03fa4cecc20126e1fd7dd4d97
sonusukh1 <64952006+sonusukh1@users.noreply.github.com>
2022-04-11 17:44:05 +0530

    Create 4.Kth Minimum Element.java

---
+++ 4.Kth Minimum Element.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+    PriorityQueue<Integer> p=new PriorityQueue<>(arr.length);
+    for(int i=0;i<arr.length;i++)
+    {
+        p.add(arr[i]);
+    
+        
+    }
+   // int times=arr.length-k;
+    
+    for(int i=0;i<k-1;i++)
+    {
+      p.remove();
+    }
+    //int t=p.remove();
+    return p.peek();
+}
+}
